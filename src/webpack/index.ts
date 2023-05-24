63bc7d4f4e8a4255eaa0ecbf6a8b07390e49c984
tujiawei <tujiawei@flashwhale.com>
2023-05-24 15:40:49 +0800

    feat

--- src/webpack/index.ts
+++ src/webpack/index.ts
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 export { default as getUmdLibConfig } from './umd-lib';
 export { default as getUmdConfig } from './umd';
-=======
-export { default as getUmdConfig } from './umd';
-export { default as getCjsConfig } from './cjs';
->>>>>>> 02c2313ee1356b9e1dbfa08f4784a7d4e6b47b86
